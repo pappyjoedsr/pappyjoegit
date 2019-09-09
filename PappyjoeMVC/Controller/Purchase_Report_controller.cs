e4a23d1d048d24a42d00d2e1b2ca0d2ab26cda0e
vandana@pappyjoe.com <vandana@pappyjoe.com>
2019-09-09 14:26:56 +0530

    .

---
+++ PappyjoeMVC/Controller/Purchase_Report_controller.cs
@@ -0,0 +1,32 @@
+﻿using PappyjoeMVC.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PappyjoeMVC.Controller
+{
+    public class Purchase_Report_controller
+    {
+        Purchase_Report_interface intr;
+        Purchase_Report_model mdl = new Purchase_Report_model();
+        Daily_Invoice_Report_model dm = new Daily_Invoice_Report_model();
+        public Purchase_Report_controller(Purchase_Report_interface inttr)
+        {
+            intr = inttr;
+            intr.setController(this);
+        }
+        public void purchdtls(string frmdte,string todte)
+        {
+            DataTable dt = mdl.purchdtls(frmdte, todte);
+            intr.purchdtls(dt);
+        }
+        public void practicedetails()
+        {
+            DataTable dt =dm.practicedetails();
+            intr.practicedetails(dt);
+        }
+    }
+}
