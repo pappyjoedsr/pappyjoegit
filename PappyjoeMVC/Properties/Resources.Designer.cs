1a25598168c158ecdd9d064f9c7ab73298afe642
irshad@pappyjoe.com <irshad@pappyjoe.com>
2019-10-14 12:37:42 +0530

    a

--- PappyjoeMVC/Properties/Resources.Designer.cs
+++ PappyjoeMVC/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace PappyjoeMVC.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap card_small {
+            get {
+                object obj = ResourceManager.GetObject("card_small", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap deleteicon {
             get {
                 object obj = ResourceManager.GetObject("deleteicon", resourceCulture);
